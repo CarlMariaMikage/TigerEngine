2ec887d857a81b6da944f0c866ee3befafc80078
CarlMariaMikage <dassayan0013@gmail.com>
2021-08-18 14:21:40 +0200

    initial commit

---
+++ include/pch.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+/** 
+ * Precompiled header
+ * 
+ * --------------------
+ * 
+ * N.B. USE FOR C++ FILES ONLY
+ * 
+ * - Saves time during compilation.
+ * - Helps make header and source files more organised.
+ * 
+ *  **/
+
+// Standard libraries
+#include <string>
+#include <vector>
+#include <thread>
+
+// OpenGL libraries
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include "pch.hpp"
+
+int main()
+{
+    glfwInit();
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    //Windowed
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Tiger Engine", nullptr, nullptr);
+    //Fullscreen
+    //GLFWwindow* window_fullscreen = glfwCreateWindow(800, 600, "OpenGL", glfwGetPrimaryMonitor(), nullptr);
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+
+        //Map ESC key to WindowShouldClose functionality
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+    
+
+    glfwTerminate();
+
+    return 0;
+}
