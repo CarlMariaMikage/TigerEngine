e1e30d9e04f69016eb3ee38a146a88bb194ba0e2
CarlMariaMikage <dassayan0013@gmail.com>
2021-08-20 08:04:04 +0200

    Rechteck

--- src/main.cpp
+++ src/main.cpp
@@ -94,19 +94,31 @@ int main()
     glDeleteShader(vertex_shader); glDeleteShader(fragment_shader);
     
     /**
-     * Vertex data and buffer setup, and vertex attribute config
+     * Vertex array, vertex buffer and element buffer config
      * **/
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {
+        0, 1, 3,   // first triangle
+        1, 2, 3    // second triangle
     };
     unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
+    glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -121,6 +133,10 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shader_program);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
         /**
          * Swap buffers and poll I/O events
          */
@@ -128,6 +144,11 @@ int main()
         glfwPollEvents();
     }
     
+    //Termination and deallocation
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO); glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shader_program);
+
     glfwTerminate();
     return 0;
 }
