90daac1ea25e50e9bab1e8a26e24f1039b3b4d62
CarlMariaMikage <dassayan0013@gmail.com>
2023-01-11 21:46:14 +0530

    Rename and cleanup

--- beta/src/main.c
+++ beta/src/main.c
@@ -1,7 +1,16 @@
-#include <test.h>
+#include <core/asserts.h>
+#include <core/logger.h>
+
+#include <stdio.h>
 
 int main(void)
 {
-    print_int(42);
-    return 0;
+    TFATAL("A test message: %f", 3.14f);
+    TERROR("A test message: %f", 3.14f);
+    TWARN("A test message: %f", 3.14f);
+    TINFO("A test message: %f", 3.14f);
+    TDEBUG("A test message: %f", 3.14f);
+    TTRACE("A test message: %f", 3.14f);
+
+    TASSERT(1 == 0);
 }
